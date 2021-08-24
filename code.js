97ceedf4ab1150770fe650cee83f2f14cc66fae7
James Livingston <james.b.livingston24@gmail.com>
2021-08-24 14:32:36 -0500

    unit tests for edge cases.

--- code.js
+++ code.js
@@ -3,12 +3,12 @@ function helloWorld() {
     return "Hello, World!";
 }
  // Pre- refactor:
-
+//
 // function sayHello(input) {
 //     if (input === "Pat") {
 //         return ("Hello, Pat!")
 //     } else if (input === "Alex") {
-//          return ("Hello, Alex!");
+//         return ("Hello, Alex!");
 //     } else {
 //         return ("Hello, Jane!");
 //     }
@@ -16,5 +16,18 @@ function helloWorld() {
 
 
 function sayHello(input) {
-    return "Hello, " + input + "!";
+    if(input === true){
+        return "Hello, World!";
+    }
+    if(input === false){
+        return "Hello, World!";
+    }
+    if(typeof input === 'undefined') {
+        return "Hello, World!";
+    }
+    else {
+        return "Hello, " + input + "!";
+    }
 }
+
+
