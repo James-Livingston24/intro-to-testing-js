a630a11a8923124a0dde4c862c8903f8a6104f41
James Livingston <james.b.livingston24@gmail.com>
2021-08-24 12:32:52 -0500

    feat: Unit Testing submission 3

--- code.js
+++ code.js
@@ -3,6 +3,12 @@ function helloWorld() {
     return "Hello, World!";
 }
 
-function sayHello() {
-    return "";
+function sayHello(input) {
+    if (input === "Pat") {
+        return ("Hello, Pat!")
+    } else if (input === "Alex") {
+         return ("Hello, Alex!");
+    } else {
+        return ("Hello, Jane!");
+    }
 }
